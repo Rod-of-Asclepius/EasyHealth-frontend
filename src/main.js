d3e1b29c7b466aa377a1765d285f4157642abe1a
water127 <1561513500@qq.com>
2021-11-02 16:42:52 +0800

    :tada: inital commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
+  store,
+  // router,
+  beforeCreate() {
+    Vue.prototype.$bus = this //安装全局事件总线
+  }
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import VueRouter from "vue-router";
+// import Count from "../components/Count"
+// import Person from "../components/Person";
+//创建并暴露一个路由器
+export default new VueRouter({
+  routes: [
+    // {
+    //   path: '/count',
+    //   component: Count
+    // },
+    // {
+    //   path: '/person',
+    //   component: Person
+    // }
+  ]
+})
